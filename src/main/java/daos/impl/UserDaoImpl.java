ca60b66514ebd3c195c54fd464fd6eaf8f74e833
DKuperN <kuperdn@gmail.com>
2016-05-02 23:00:31 +0300

    db connection

--- src/main/java/daos/impl/UserDaoImpl.java
+++ src/main/java/daos/impl/UserDaoImpl.java
@@ -3,9 +3,13 @@ package daos.impl;
 import daos.UserDAO;
 import models.UserModel;
 
+import javax.sql.DataSource;
 import java.util.Random;
 
 public class UserDaoImpl implements UserDAO<UserModel> {
+
+    private DataSource dataSource;
+
 
     public void saveNewUser(String userName, String userEmail) {
         System.out.println("User " + userName + " with " + userEmail + " WILL be save in DB (not ready schema)");
