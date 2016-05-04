9877549787bbc8381fb727419f57eecd3d5f5fd2
Dzianis Kupryianchyk <dzianis_kupryianchyk@epam.com>
2016-05-04 10:17:50 +0300

    db schema

--- src/main/resources/db_buc.sql
+++ src/main/resources/db_buc.sql
@@ -9,9 +9,47 @@
 /*!40101 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
--- Data exporting was unselected.
+
+-- Dumping database structure for theatredb
+CREATE DATABASE IF NOT EXISTS `theatredb` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `theatredb`;
+
+
+-- Dumping structure for table theatredb.event
+CREATE TABLE IF NOT EXISTS `event` (
+  `eventID` int(11) NOT NULL AUTO_INCREMENT,
+  `eventName` varchar(50) NOT NULL DEFAULT '0',
+  `eventPlace` varchar(50) NOT NULL DEFAULT '0',
+  `eventDate` date NOT NULL,
+  `eventStartTime` time NOT NULL DEFAULT '00:00:00',
+  `eventEndTime` time NOT NULL DEFAULT '00:00:00',
+  `priceForTicket` double NOT NULL DEFAULT '0',
+  `rating` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`eventID`),
+  KEY `rating` (`rating`),
+  CONSTRAINT `FK_event_event_rating` FOREIGN KEY (`rating`) REFERENCES `event_rating` (`ratID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Data exporting was unselected.
+
+
+-- Dumping structure for table theatredb.event_rating
+CREATE TABLE IF NOT EXISTS `event_rating` (
+  `ratID` int(11) NOT NULL AUTO_INCREMENT,
+  `rating` varchar(50) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ratID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table theatredb.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `userId` int(11) NOT NULL AUTO_INCREMENT,
+  `userName` varchar(50) NOT NULL DEFAULT '0',
+  `userEmail` varchar(50) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Data exporting was unselected.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
