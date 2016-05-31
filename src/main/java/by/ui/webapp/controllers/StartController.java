9c43d852b87e1dc68866269c7c2edd376b162b75
DKuperN <kuperdn@gmail.com>
2016-05-31 10:50:14 +0300

    Spring MVC base working app

---
+++ src/main/java/by/ui/webapp/controllers/StartController.java
@@ -0,0 +1,15 @@
+package by.ui.webapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class StartController {
+
+    @RequestMapping("/")
+    public ModelAndView showStartPage(){
+        return new ModelAndView("index");
+    }
+}
